f7be0b02c51bcf3b6fd80b6174a648a34774f9a6
Dekoder-py <kyle@codingcorner.dev>
2025-11-27 22:50:15 +1300

    oops

--- flake.nix
+++ flake.nix
@@ -29,6 +29,9 @@
           "firefox"
           "ghostty"
           "home-assistant"
+          "setapp"
+          "alfred"
+          "steam"
         ];
       };
       environment.systemPackages = [
