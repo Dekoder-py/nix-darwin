bc5c98202d7445d637ab9258d46725adbfee5f61
Dekoder-py <kyle@codingcorner.dev>
2025-12-15 16:18:07 +1300

    fix import paths

--- hosts/carbon.nix
+++ hosts/carbon.nix
@@ -1,12 +1,12 @@
-{...}: let
+{username, ...}: let
 in {
   system.primaryUser = username;
   users.users.kyle.home = /Users/kyle;
 
   imports = [
-    ./modules/base.nix
-    ./modules/homebrew.nix
-    ./modules/system.nix
-    ./modules/packages.nix
+    ../modules/base.nix
+    ../modules/homebrew.nix
+    ../modules/system.nix
+    ../modules/packages.nix
   ];
 }
