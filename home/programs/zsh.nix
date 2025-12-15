f0e36ad6d897a19db08d138face53c658795472e
Dekoder-py <kyle@codingcorner.dev>
2025-12-16 12:37:02 +1300

    no lazy shortcuts anymore :D

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -36,9 +36,6 @@
       ld = "eza -lhD --icons=always";
       lt = "eza --icons=always --tree";
       src = "source ~/.zshrc";
-
-      update-flake = "cd /etc/nix-darwin && nix flake update && git add flake.lock && git commit && git push";
-      rebuild = "sudo darwin-rebuild switch";
     };
 
     initContent = ''
