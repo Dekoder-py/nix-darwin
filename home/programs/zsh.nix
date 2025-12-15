aac5f57ca8acf217b1855aa76a9a7bb945f347ee
Dekoder-py <kyle@codingcorner.dev>
2025-12-15 17:35:52 +1300

    update zsh

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -18,6 +18,34 @@
       ".." = "cd ..";
       "..." = "cd ...";
       lg = "lazygit";
+      l = "eza -lh --icons=always";
+      ls = "eza -1 --icons=always";
+      ll = "eza -lha --icons=always --sort=name --group-directories-first";
+      ld = "eza -lhD --icons=always";
+      lt = "eza --icons=always --tree";
     };
+
+    initExtra = ''
+      set -o vi
+
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "${s.:. LS_COLORS}"
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+
+      [ "$TERM" = "xterm-kitty" ] && alias ssh='kitty +kitten ssh' # fix ssh in kitty
+      if [[ "$TERM" == "xterm-ghostty" ]]; then
+      alias ssh='TERM=xterm-256color ssh'
+      fi
+
+      eval "$(zoxide init zsh --cmd cd)"
+
+      export FZF_DEFAULT_COMMAND='fd --hidden --exclude Library --exclude Applications --exclude ".cache"' # fzf will use fd, inculding hidden and excluding large folders
+      # The following lines have been added by Docker Desktop to enable Docker CLI completions.
+      fpath=(/Users/kyle/.docker/completions $fpath)
+      autoload -Uz compinit
+      compinit
+      # End of Docker CLI completions
+    '';
   };
 }
