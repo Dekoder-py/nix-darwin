e1930896ad2433ac8a10b210109f5ff80d5f03fa
Dekoder-py <kyle@codingcorner.dev>
2025-12-16 14:46:34 +1300

    fix theme

--- home/programs/bat.nix
+++ home/programs/bat.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.bat = {
     enable = true;
     themes = {
@@ -9,7 +9,7 @@
           rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
           hash = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
         };
-        file = "catppuccin.tmTheme";
+        file = "themes/Catppuccin Mocha.tmTheme";
       };
     };
     config = {
