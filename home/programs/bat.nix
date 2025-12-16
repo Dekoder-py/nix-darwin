96a2d22d6ebc5e9c874cffd77596923c88cd135d
Dekoder-py <kyle@codingcorner.dev>
2025-12-16 14:41:57 +1300

    add hash

--- home/programs/bat.nix
+++ home/programs/bat.nix
@@ -7,6 +7,7 @@
           owner = "catppuccin";
           repo = "bat";
           rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
+          hash = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
         };
         file = "Catppuccin Mocha.tmTheme";
       };
