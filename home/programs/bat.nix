5123926fd57360ca39987fc88da65d76bb4a41b5
Dekoder-py <kyle@codingcorner.dev>
2025-12-16 14:41:14 +1300

    add rev

--- home/programs/bat.nix
+++ home/programs/bat.nix
@@ -2,10 +2,11 @@
   programs.bat = {
     enable = true;
     themes = {
-      dracula = {
+      catppuccin = {
         src = pkgs.fetchFromGitHub {
           owner = "catppuccin";
           repo = "bat";
+          rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
         };
         file = "Catppuccin Mocha.tmTheme";
       };
