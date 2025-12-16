faca86dc179bcc2f680d937f1c656b5626e45758
Dekoder-py <kyle@codingcorner.dev>
2025-12-16 17:45:36 +1300

    more tmux config

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -11,13 +11,22 @@
       tmux-fzf
       yank
       resurrect
+      minimal-tmux-status
     ];
 
     extraConfig = ''
+      set -g status-position top
+      set -g renumber-window on
+      set -g status-style bg=default
       set -g status-interval 5
       set -g status-left-length 99
       set -g status-right-length 99
       set -g status-justify absolute-centre
+
+      bind-key h select-pane -L
+      bind-key j select-pane -D
+      bind-key k select-pane -U
+      bind-key l select-pane -R
     '';
   };
 }
