e5cf18b380fbc7db95497f7f94736ccf8cdf0b22
Dekoder-py <dekod3r.py@gmail.com>
2025-12-17 19:52:32 +1300

    add something fun

--- home/default.nix
+++ home/default.nix
@@ -1,9 +1,18 @@
-{ pkgs, username, ... }:
-
 {
+  pkgs,
+  username,
+  ...
+}: {
   home.username = username;
   home.homeDirectory = "/Users/${username}";
   home.stateVersion = "25.11";
+
+  # This thing lets me fuzzy search options - source: https://www.tonybtw.com/tutorial/nixos-hyprland/#nix-search-tv (and copilot for fixing bug)
+  home.packages = with pkgs; [
+    (pkgs.writeShellScriptBin "ns" (builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh"))
+    fzf
+    nix-search-tv
+  ];
 
   imports = [
     ./programs/git.nix
@@ -18,4 +27,3 @@
     ./programs/fastfetch.nix
   ];
 }
-
