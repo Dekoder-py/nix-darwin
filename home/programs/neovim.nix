bdb48b97e31032babb094c21c9c7f69aa71c78b6
Dekoder-py <kyle@codingcorner.dev>
2025-12-28 18:32:09 +1300

    fixes and plugins and indents

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -1,17 +1,26 @@
 {
   programs.nixvim = {
     enable = true;
+
+    opts = {
+      tabstop = 2;      
+      shiftwidth = 2;   
+      expandtab = true; 
+    };
+
     colorschemes.catppuccin.enable = true;
 
     statusline.lualine.enable = true;
 
     plugins = {
-    treesitter = {
-    enable = true;
-    highlight.enable = true;
-    indent.enable = true;
+        treesitter = {
+            enable = true;
+            highlight.enable = true;
+            indent.enable = true;
+        };
+        indent-blankline = {
+            enable = true;
+        };
     };
-  };
-
   };
 }
