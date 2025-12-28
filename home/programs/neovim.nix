aab4c2a69cc11e509e211a369229d0909234fd81
Dekoder-py <kyle@codingcorner.dev>
2025-12-28 18:40:58 +1300

    keymaps

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -2,6 +2,9 @@
   programs.nixvim = {
     enable = true;
 
+    globals.mapleader = " ";
+    globals.maplocalleader = " ";
+
     opts = {
       tabstop = 2;      
       shiftwidth = 2;   
@@ -24,5 +27,18 @@
       lazygit.enable = true;
       telescope.enable = true;
     };
+
+    keymaps = [
+    {
+      mode = "n";
+      key = "<leader><space>";
+      action = "<cmd>Telescope find_files<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Find files (Telescope)";
+      };
+    }
+  ];
   };
 }
