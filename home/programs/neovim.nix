af7df2dac678424e9514e887fb3114f07a0bcaa3
Dekoder-py <kyle@codingcorner.dev>
2025-12-28 18:21:54 +1300

    add treesitter

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -5,5 +5,14 @@
 
     statusline.lualine.enable = true;
 
+    plugins = {
+    treesitter = {
+    enable = true;
+    highlight.enable = true;
+    indent.enable = true;
+    folding.enable = true;
+    };
+  };
+
   };
 }
