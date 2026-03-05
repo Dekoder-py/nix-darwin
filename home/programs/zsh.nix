a5cebc9c0890891a7dba6fe1178f2e4393e6f251
Dekoder-py <kyle@codingcorner.dev>
2026-03-05 16:52:43 +1300

    add cargo bin to path

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -4,7 +4,7 @@
 
     sessionVariables = {
       TERM = "xterm-256color";
-      PATH = "$HOME/go/bin:$HOME/.local/bin:$PATH";
+      PATH = "$HOME/.cargo/bin:$HOME/go/bin:$HOME/.local/bin:$PATH";
       FZF_DEFAULT_OPTS = " \
         --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
         --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
